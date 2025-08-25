0cdd6cee2f81ee101e4921b6e253a63f5b0290f0
e_sarsenovv <cool.yernar.sarsenov@gmail.com>
2025-08-25 15:50:07 +0500

    sfdffds

--- multidimensional_arrays.cpp
+++ multidimensional_arrays.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 
+using namespace std;
+
+int main() {
+
+    string cars[][3] = {{"Mustang", "Escape", "F-150"},
+                       {"Corvette", "Cobalt", "Camry"},
+                       {"AMG", "Challenger", "Ram 1500"}};
+    
+    int rows = sizeof(cars)/sizeof(cars[0]);
+    int columns = sizeof(cars[0])/sizeof(cars[0][0]);
+
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            cout << cars[i][j] << " ";
+        }
+        cout << endl;
+    }
+        
+    
+
+
+    return 0;
+
+}
